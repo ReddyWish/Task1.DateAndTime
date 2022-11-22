7cf1a614996592f798446fa2fca1b9c6258c356a
Ilya <ilya.shuvatov1@gmail.com>
2022-11-22 10:33:29 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+function getDateFormat(date, separator) {
+    date = new Date()
+    let year = date.getFullYear()
+    let month = date.getMonth()
+    if (month < 10) {
+        month = '0' + month
+    }
+    let day = date.getDate()
+    if (day < 10) {
+        day = '0' + day
+    }
+    let finalDate = day + separator + month + separator + year
+    
+    return finalDate
+}
+
+console.log(getDateFormat('какое сегодня число?', '/'))
+
